d548bfccfa524e5adb08dbc00e8894e5fe9752de
Maje97 <maje@hotmail.se>
2023-12-22 16:04:16 +0100

    Forts. sten sax påse
    Har fått nästan allt att funka förutom att ändra inre HTML.

--- stensaxpase.js
+++ stensaxpase.js
@@ -1,29 +1,60 @@
 let val ='';
 let speletsval ='';
 let randomval ='';
+let resultat ='';
+
+let score = {
+    vinster: 0,
+    forluster: 0,
+    oavgjort: 0,
+}
+
 
 function startaSpel(val) {
     let randomval = Math.random()
 
     if (randomval < 0.33){
         speletsval = 'sten';
-    } else if(randomval > 0.33 && <= 0.66){
+    } else if(randomval > 0.33 && randomval <= 0.66){
         speletsval = 'sax';
     } else {
         speletsval = 'påse';
     }
 
+    console.log('Du valde:', val);
+    console.log('Spelet valde:', speletsval);
+
     switch(val){
         case 'sten':
             switch(speletsval){
-                case 'sten': break;
-                case 'sax': break;
-                case 'påse': break;
+                case 'sten': resultat='Oavgjort!'; score.oavgjort+=1; break;
+                case 'sax': resultat='Du vann!'; score.vinster+=1; break;
+                case 'påse': resultat='Du förlorade!'; score.forluster+=1; break;
             }
         break;
         case 'sax':
+            switch(speletsval){
+                case 'sten': resultat='Du förlorade!'; score.forluster+=1; break;
+                case 'sax': resultat='Oavgjort!'; score.oavgjort+=1; break;
+                case 'påse': resultat='Du vann!'; score.vinster+=1; break;
+            }
         break;
         case 'påse':
+            switch(speletsval){
+                case 'sten': resultat='Du vann!'; score.vinster+=1; break;
+                case 'sax': resultat='Du förlorade!'; score.forluster+=1; break;
+                case 'påse': resultat='Oavgjort!'; score.oavgjort+=1; break;
+            }
         break;
+    }
+    console.log(score);
+    alert(`Du valde ${val}, spelet valde ${speletsval}. ${resultat}`);
+}
+
+function aterstall() {
+    score = {
+        vinster: 0,
+        forluster: 0,
+        oavgjort: 0,
     }
 }
