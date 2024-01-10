ffb7e966b248aa01192c17aec9c7cefd6b373596
Maje97 <maje@hotmail.se>
2024-01-10 13:31:44 +0100

    Kommit längre på visual novel efter hjälp

--- visualnovel.js
+++ visualnovel.js
@@ -1,9 +1,18 @@
 const background = document.querySelector('.js-background');
-const character = document.querySelector('.js-character');
+const charleft = document.querySelector('.js-charleft');
+const charmiddle = document.querySelector('.js-charmiddle');
+const charright = document.querySelector('.js-charright');
+const narrator = document.querySelector('js-narrator');
 const textbox = document.querySelector('js-textbox');
+const buttonbox = document.querySelector('js-buttonbox');
 const UI = document.querySelector('js-ui');
 
 function startGame() {
     background.innerHTML = `<img class="vn-background" src="visualnovel-assets/backgrounds/cave.png">`;
-    character.innerHTML = `<img class="vn-charleft" src="visualnovel-assets/characters/GRC2018005_001.png>`;
+    charleft.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+    charmiddle.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+    charright.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+    narrator.innerHTML = `<h1>Välkommen</h1>`;
+    textbox.innerHTML = `<h1>Hej</h1>`;
+    buttonbox.innerHTML = ``;
 }
