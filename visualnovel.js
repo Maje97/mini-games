3f3a348a9d90c482fa8ba31366cf9c0ef9fee192
Maje97 <maje@hotmail.se>
2024-01-15 15:21:59 +0100

    Visual novel
    Börjat jobba lite med visual novel. Påbörjat switch-sats.

--- visualnovel.js
+++ visualnovel.js
@@ -15,11 +15,50 @@ function startGame() {
     charright.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
     narrator.innerHTML = `<h2>Hej</h2>`;
     textbox.innerHTML = `<p class="vnp">hej</p>`;
-    buttonbox.innerHTML = `<button class="nextbutton" onclick="nextSlide()">Next</button>`;
-    
+    buttonbox.innerHTML = `<button class="nextbutton" onclick="nextSlide()"></button>`;
 }
 
 function nextSlide() {
     slide++;
+
+    switch(slide) {
+        case 1:     
+            background.innerHTML = `<img class="vn-background" src="visualnovel-assets/backgrounds/cave.png">`;
+            charleft.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+            charmiddle.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+            charright.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+            narrator.innerHTML = `<h2></h2>`;
+            textbox.innerHTML = `<p class="vnp"></p>`;
+        break;
+        case 2: 
+            background.innerHTML = `<img class="vn-background" src="visualnovel-assets/backgrounds/cave.png">`;
+            charleft.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+            charmiddle.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+            charright.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+            narrator.innerHTML = `<h2></h2>`;
+            textbox.innerHTML = `<p class="vnp"></p>`;
+        break;
+        case 3: 
+            background.innerHTML = `<img class="vn-background" src="visualnovel-assets/backgrounds/cave.png">`;
+            charleft.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+            charmiddle.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+            charright.innerHTML = `<img class="vn-char" src="visualnovel-assets/characters/GRC2018005_001.png">`;
+            narrator.innerHTML = `<h2></h2>`;
+            textbox.innerHTML = `<p class="vnp"></p>`;
+        break;
+        default:
+            background.innerHTML = ``;
+            charleft.innerHTML = ``;
+            charmiddle.innerHTML = ``;
+            charright.innerHTML = ``;
+            narrator.innerHTML = `<h2>Character sprites: Spritebase on itch.io.</h2>
+            <h2>Backgrounds: Uncle Matt on itch.io.</h2>
+            <h2>Design start button: Wenrexa on itch.io.</h2>
+            <h2>Design play button: Iconduck.</h2>`;
+            textbox.innerHTML = `<p class="vnp">Tack för att du spelat! Ladda om sidan om du vill spela igen.</p>`;
+            buttonbox.innerHTML = ``;
+        break;
+    }
+
     console.log(slide);
 }
